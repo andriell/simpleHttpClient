ed1be4339993aabe352ab07b3e2cfb76e0e87d2f
andriell <root.andriell@gmail.com>
2014-09-18 00:42:19 +0400

    Коммент

--- src/http/client/HttpClientCacheDir.java
+++ src/http/client/HttpClientCacheDir.java
@@ -147,24 +147,6 @@ public class HttpClientCacheDir implements HttpClientCache {
         return 0;
     }
 
-    public void walk( String path ) {
-
-        File root = new File( path );
-        File[] list = root.listFiles();
-
-        if (list == null) return;
-
-        for ( File f : list ) {
-            if ( f.isDirectory() ) {
-                walk( f.getAbsolutePath() );
-                System.out.println( "Dir:" + f.getAbsoluteFile() );
-            }
-            else {
-                System.out.println( "File:" + f.getAbsoluteFile() );
-            }
-        }
-    }
-
     //<editor-fold desc="Getters and Setters">
     public String getDirForDefaultUser() {
         return dirForDefaultUser;
